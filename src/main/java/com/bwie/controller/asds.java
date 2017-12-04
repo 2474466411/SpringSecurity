c35a2227a7859505c6d319cafc481c504de03e75
2474466411 <git>
2017-12-04 21:28:01 +0800

    轻悄悄啊

--- src/main/java/com/bwie/controller/asds.java
+++ src/main/java/com/bwie/controller/asds.java
@@ -4,4 +4,10 @@ package com.bwie.controller;
  * Created by Administrator on 2017/12/4.
  */
 public class asds {
+    public static void main(String[] args) {
+        System.out.println("nnn");
+        System.out.println("nnn");
+        System.out.println("nnn");
+        System.out.println("nnn");
+    }
 }
